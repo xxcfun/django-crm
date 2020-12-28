5bf466b8d38e52bceb12e021059b93e2646f7223
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2020-12-28 09:28:11 +0800

    '修改拜访记录模型'

--- record/admin.py
+++ record/admin.py
@@ -6,6 +6,6 @@ from record.models import Record
 
 @admin.register(Record)
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'status', 'user', 'main', 'next', 'remarks',
+    list_display = ('theme', 'customer', 'status', 'user', 'main', 'next', 'remarks',
                     'created_at', 'updated_at', 'is_valid')
     list_per_page = 10
--- record/models.py
+++ record/models.py
@@ -8,20 +8,21 @@ from utils import constants
 
 class Record(models.Model):
     """客户拜访记录模型"""
+    theme = models.CharField(verbose_name='拜访主题', max_length=256)
     customer = models.ForeignKey(Customer, verbose_name='客户', related_name='customer_record', on_delete=models.CASCADE)
     status = models.SmallIntegerField(verbose_name='客户拜访方式', choices=constants.RECORD_STATUS, default=constants.STATUS_XS)
     user = models.ForeignKey(User, verbose_name='创建人', related_name='customer_record', on_delete=models.CASCADE)
-    main = models.TextField('主要事宜', max_length=1000, null=True, blank=True)
-    next = models.TextField('后期规划', max_length=1000, null=True, blank=True)
-    remarks = models.CharField('拜访备注', max_length=255, blank=True, null=True)
+    main = models.TextField(verbose_name='主要事宜', max_length=1000, null=True, blank=True)
+    next = models.TextField(verbose_name='后期规划', max_length=1000, null=True, blank=True)
+    remarks = models.CharField(verbose_name='拜访备注', max_length=255, blank=True, null=True)
 
-    created_at = models.DateTimeField('创建时间', auto_now_add=True)
-    updated_at = models.DateTimeField('修改时间', auto_now=True)
-    is_valid = models.BooleanField('是否有效', default=True)
+    created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='修改时间', auto_now=True)
+    is_valid = models.BooleanField(verbose_name='是否有效', default=True)
 
     class Meta:
         db_table = 'record'
         verbose_name = verbose_name_plural = "客户拜访记录"
 
     def __str__(self):
-        return self.customer.name
+        return self.theme
